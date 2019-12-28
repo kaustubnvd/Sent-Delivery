92730c45aec5d7140b7bc59b4dd34a72e648f967
Kaustub <kaustub.nvd@gmail.com>
2019-12-28 14:41:12 -0600

    Added Circular Tabs Widget

---
+++ lib/widgets/circular_tabs.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:circular_bottom_navigation/circular_bottom_navigation.dart';
+
+import '../providers/tabs.dart';
+
+/* 
+    Authors: Kaustub Navalady, Last Edit: 12/28/19
+*/
+
+class CircularTabs extends StatefulWidget {
+  @override
+  _CircularTabsState createState() => _CircularTabsState();
+}
+
+class _CircularTabsState extends State<CircularTabs> {
+  CircularBottomNavigationController _navigationController; // Controls the tab navigation
+  var _selectedPos = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _navigationController = CircularBottomNavigationController(_selectedPos);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var _tabItems = Provider.of<Tabs>(context, listen: false).tabItems;
+    return Column(
+      children: <Widget>[
+        CircularBottomNavigation(
+          _tabItems,
+          controller: _navigationController,
+          barBackgroundColor: Theme.of(context).primaryColor,
+          animationDuration: Duration(milliseconds: 300),
+          selectedCallback: (int selectedPos) {
+            setState(() {
+              this._selectedPos = selectedPos;
+            });
+          },
+        ),
+      ],
+    );
+  }
+}
